db54a2212615d3a2b813b236f450359b91a09c1f
Aleksander Froncz <aleksander.froncz@gmail.com>
2018-04-28 18:54:15 +0200

    Updates

---
+++ content.js
@@ -0,0 +1,35 @@
+window.content = {
+  "o_nas": {
+    "podtytul": "Ochroną informacji i zabezpieczeniami zajmuję się od ponad 10 lat.",
+    "doswiadczenie": "Dotychczasowe doświadczenie zdobyłem w obszarze administracji rządowej i biznesie pełniąc funkcje:",
+    funkcje: [
+      "Oficera Bezpieczeństwa",
+      "Pełnomocnika do spraw ochrony informacji niejawnych",
+      "Administratora bezpieczeństwa informacji",
+      "Inspektora bezpieczeństwa teleinformatycznego",
+      "Inspektora bezpieczeństwa fizycznego"
+    ],
+    "certyfikaty": "Posiadam niezbędne szkolenia i certyfikaty."
+  },
+  "oferta": {
+    "tytul": "Oferta",
+    "nasza_specjalnosc": "Naszą specjalnością jest świadczenie usług konsultingowych i doradczych związanych z ochroną informacji niejawnych, danych osobowych oraz tajemnicy przedsiębiorstwa. Oferujemy usługi w zakresie doboru zabezpieczeń fizycznych, elektronicznych i bezpieczeństwa systemów teleinformatycznych.",
+    "wsparcie": "Udzielamy wsparcia w zakresie:",
+    "zakres": [
+      'uzyskania świadectwa bezpieczeństwa przemysłowego',
+      'uzyskania akredytacji bezpieczeństwa teleinformatycznego',
+      'opracowania i wdrożenia polityki bezpieczeństwa',
+      'opracowania i implementacji planu ochrony',
+      'szkolenia z zakresu ochrony danych osobowych i informacji niejawnych'
+    ],
+    "uslugi": "Świadczymy usługi związane z utrzymaniem i instalacją systemów bezpieczeństwa."
+  },
+  "kontakt": {
+    "tytul": "Kontakt",
+    "nazwa": "Tomasz Puta",
+    "adres1": "ul. Żytnia 26/64",
+    "adres2": "08-110 Siedlce",
+    "email": "biuro@kontecho.pl",
+    "numer": "604105315"
+  }
+}
